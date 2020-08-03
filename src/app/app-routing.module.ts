4b00a3b193e5e2119cf7e1f430fcd5e792d73821
joanna <okokis101@gmail.com>
2020-08-03 18:15:57 +0800

    route v1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,51 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [ // appcomponent的<router-outlet>
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '', // 空的或隨便打都會導向home的頁面
+    redirectTo: 'spa/home',
+    pathMatch: 'full' // 使用redirectTo就一定要使用pathMatch
+  },
+  {
+    path: 'spa',
+    component: LayoutComponent, // 多一個children就要多一個在component裡的<router-outlet>
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' }, // 只打spa也導向home
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'about',
+        component: AboutComponent
+      }
+    ]
+  }
+  // {
+  //   path: 'home',
+  //   component: HomeComponent
+  // },
+  // {
+  //   path: 'about',
+  //   component: AboutComponent
+  // }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    enableTracing: true, // 上正式機要拿掉，會自動log出來
+    useHash: true
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+// Module
+import { FeatureModule } from './feature/feature.module';
+// Component
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    LayoutComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    FeatureModule,
     AppRoutingModule
   ],
   providers: [],
